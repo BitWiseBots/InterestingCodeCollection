f7c0b2859e2ac17d7cd58ed77761ef58d3e7a485
Justin Madill <justin.madill@gmail.com>
2016-05-26 17:27:22 -0600

    Updated Triangle project

--- TrianglePattern.Wpf/MainWindow.xaml.cs
+++ TrianglePattern.Wpf/MainWindow.xaml.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Sandbox.Wpf
 {
@@ -12,40 +16,71 @@ namespace Sandbox.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const double BaseLength = 1;
-        private const double ScaleFactor = 1.102;
-
-        private readonly double _centerX;
-        private readonly double _centerY;
-
-        private readonly List<Polygon> _previousTriangles;
+        private TaskScheduler _scheduler;
 
         public MainWindow()
         {
-            _previousTriangles = new List<Polygon>();
             InitializeComponent();
+            _scheduler = TaskScheduler.FromCurrentSynchronizationContext();
+        }
 
-            _centerX = drawingCanvas.Width / 2;
-            _centerY = drawingCanvas.Height / 2;
+        private void DrawingCanvas_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            var position = e.GetPosition(drawingCanvas);
 
-            for (var i = 0; i < 100; i++)
+
+            Task.Factory.StartNew(() => { })
+                .ContinueWith((task, o) => PatternRenderer.DrawPattern(position, drawingCanvas), null, _scheduler);
+
+            //Task.Run(() => PatternRenderer.DrawPattern(position, drawingCanvas));
+        }
+    }
+
+    public class PatternRenderer
+    {
+        private const double BaseLength = 1;
+        private const double ScaleFactor = 1.102;
+
+        private double _centerX;
+        private double _centerY;
+
+        private int _currIterations;
+
+        private readonly List<Polygon> _previousTriangles;
+        private readonly Canvas _drawingCanvas;
+
+        private PatternRenderer(Canvas canvas)
+        {
+            _previousTriangles = new List<Polygon>();
+            _drawingCanvas = canvas;
+        }
+
+        public static async void DrawPattern(Point origin, Canvas canvas)
+        {
+            var renderer = new PatternRenderer(canvas);
+            renderer._centerX = origin.X;
+            renderer._centerY = origin.Y;
+
+            renderer.AddTriangle(true);
+
+            for (var i = 0; i < 99; i++)
             {
-                AddTriangle();
+                renderer.AddTriangle(false);
+                await Task.Delay(100);
             }
         }
 
-        private void AddTriangle()
+        private void AddTriangle(bool isNew)
         {
             var newTriangle = new Polygon
             {
                 Stroke = Brushes.Black,
                 StrokeThickness = 1,
-                Fill = Brushes.Black,
-                Opacity = 0.5
             };
 
-            if (_previousTriangles.Count == 0)
+            if (isNew)
             {
+                _currIterations = 0;
                 newTriangle.Points.Add(new Point(_centerX, _centerY));
                 newTriangle.Points.Add(new Point(_centerX, _centerY + BaseLength));
                 newTriangle.Points.Add(new Point(_centerX + BaseLength, _centerY));
@@ -56,7 +91,7 @@ namespace Sandbox.Wpf
                 var prevAngle = Math.Atan2(prev.Points[0].Y - prev.Points[1].Y, prev.Points[0].X - prev.Points[1].X) * 180 / Math.PI;
                 var newAngle = prevAngle + 45;
 
-                var newLength = BaseLength * Math.Pow(ScaleFactor, _previousTriangles.Count);
+                var newLength = BaseLength * Math.Pow(ScaleFactor, _currIterations);
 
                 var newBX = Math.Cos(newAngle * Math.PI / 180) * newLength + prev.Points[0].X;
                 var newBY = Math.Sin(newAngle * Math.PI / 180) * newLength + prev.Points[0].Y;
@@ -69,8 +104,10 @@ namespace Sandbox.Wpf
                 newTriangle.Points.Add(new Point(newCX, newCY));
             }
 
-            drawingCanvas.Children.Add(newTriangle);
+            Dispatcher.CurrentDispatcher.Invoke(() => _drawingCanvas.Children.Add(newTriangle));
+            
             _previousTriangles.Add(newTriangle);
+            _currIterations++;
         }
     }
 }
