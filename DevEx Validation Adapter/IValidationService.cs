558d82aaace387c6b912ca16d078a16c106ceb2e
BitWiseBots <justin.madill@gmail.com>
2022-01-13 11:48:02 -0700

    Delete DevEx Validation Adapter directory

--- DevEx Validation Adapter/IValidationService.cs
+++
@@ -1,9 +0,0 @@
-namespace Eka.Common.Wpf.Behaviors
-{
-    public interface IValidationService
-    {
-        bool HasValidationError { get; }
-        void UpdateValidStatus(string propertyName, bool isValid);
-        void UpdateValidStatus(int row, string propertyName, bool isValid);
-    }
-}
